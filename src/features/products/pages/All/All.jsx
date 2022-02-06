e0f0d3008bb347a0b78098def441c7c7b53a9e4c
Stewie Dong <donghuuhieu1520@gmail.com>
2022-02-06 15:57:51 +0700

    feat: dashboard
    Signed-off-by: Stewie Dong <donghuuhieu1520@gmail.com>

--- src/features/products/pages/All/All.jsx
+++ src/features/products/pages/All/All.jsx
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 // icons
 import {
@@ -7,17 +7,26 @@ import {
     PlusIcon,
     UserGroupIcon,
     ShoppingCartIcon,
+    SelectorIcon,
+    CheckIcon,
 } from '@heroicons/react/solid';
 
 // components
+import { Listbox, Transition } from '@headlessui/react';
 import { ChartViewsAndSells } from '@/features/products/components';
 
 // hooks
 import { useDocumentTitle } from '@/hooks';
 
-const ProductAllPage = () => {
+const chartViewSalesOptions = [
+    { name: 'This week', value: 'week' },
+    { name: 'This month', value: 'month' },
+];
 
+const ProductAllPage = () => {
     useDocumentTitle('Dashboard');
+
+    const [chartViewSaleSelectedOption, setChartViewSaleSelectedOption] = useState(chartViewSalesOptions[0]);
 
     return (
         <Fragment>
@@ -215,10 +224,56 @@ const ProductAllPage = () => {
                     <div className="bg-white shadow-lg rounded-sm border border-gray-200">
                         <div className="flex justify-between p-5">
                             <p className="text-dark-blue font-semibold font-sm">Views & Sells</p>
-                            <select className="text-sm text-gray-400 py-1 px-2 rounded-sm bg-gray-100">
-                                <option selected>This week</option>
-                                <option>This months</option>
-                            </select>
+                            <Listbox value={chartViewSaleSelectedOption} onChange={setChartViewSaleSelectedOption}>
+                                <div className="relative">
+                                    <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-gray-100 rounded-lg cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+                                        <span className="block truncate">{chartViewSaleSelectedOption.name}</span>
+                                        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                                            <SelectorIcon
+                                                className="w-5 h-5 text-gray-400"
+                                                aria-hidden="true"
+                                            />
+                                        </span>
+                                    </Listbox.Button>
+                                    <Transition
+                                        as={Fragment}
+                                        leave="transition ease-in duration-100"
+                                        leaveFrom="opacity-100"
+                                        leaveTo="opacity-0"
+                                    >
+                                        <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                            {chartViewSalesOptions.map((option, idx) => (
+                                                <Listbox.Option
+                                                    key={idx}
+                                                    className={({ active }) => `${active ? 'text-blue-900 bg-blue-100' : 'text-gray-900'}
+                                                    cursor-default select-none relative py-2 pl-10 pr-4`}
+                                                    value={option}>
+                                                    {({ selected, active }) => (
+                                                        <>
+                                                            <span
+                                                                className={`${selected ? 'font-medium' : 'font-normal'
+                                                                    } block truncate`}
+                                                            >
+                                                                {option.name}
+                                                            </span>
+                                                            {selected ? (
+                                                                <span
+                                                                    className={`${active ? 'text-blue-600' : 'text-blue-600'
+                                                                        }
+                                absolute inset-y-0 left-0 flex items-center pl-3`}
+                                                                >
+                                                                    <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                                                                </span>
+                                                            ) : null}
+
+                                                        </>
+                                                    )}
+                                                </Listbox.Option>
+                                            ))}
+                                        </Listbox.Options>
+                                    </Transition>
+                                </div>
+                            </Listbox>
                         </div>
                         <div className="flex w-full h-[400px] px-5 pb-5 overflow-auto">
                             <ChartViewsAndSells />
--- src/layouts/header/index.jsx
+++ src/layouts/header/index.jsx
@@ -1,9 +1,12 @@
 import { useSelector } from 'react-redux';
+import { Popover, Transition } from '@headlessui/react';
 
 import { selectLayout } from '@/store/slices/layoutSlice';
 
 import { BellIcon, ChatIcon, ChevronDownIcon } from '@heroicons/react/outline';
 import { SearchIcon } from '@heroicons/react/solid';
+import { Fragment } from 'react';
+import cx from 'classnames';
 
 const Header = () => {
     const { header } = useSelector(selectLayout);
@@ -29,12 +32,74 @@ const Header = () => {
                     </div>
                     {/* notification, chat */}
                     <div className="ml-auto flex space-x-3">
-                        <button className="flex items-center justify-center bg-transparent text-gray-400 w-10 h-10 rounded-full">
-                            <BellIcon className="w-6 h-6" />
-                        </button>
-                        <button className="flex items-center justify-center bg-transparent text-gray-400 w-10 h-10 rounded-full">
-                            <ChatIcon className="w-6 h-6" />
-                        </button>
+                        <Popover className="relative">
+                            {({ open }) => (
+                                <>
+                                    <Popover.Button className={
+                                        cx(
+                                            'flex items-center justify-center bg-transparent text-gray-400 w-10 h-10 rounded-full',
+                                            open ? 'text-blue-400 hover:text-blue-300' : 'hover:text-gray-300',
+                                        )
+                                    }>
+                                        <BellIcon className="w-6 h-6" />
+                                    </Popover.Button>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="opacity-0 translate-y-1"
+                                        enterTo="opacity-100 translate-y-0"
+                                        leave="transition ease-in duration-150"
+                                        leaveFrom="opacity-100 translate-y-0"
+                                        leaveTo="opacity-0 translate-y-1">
+                                        <Popover.Panel className="absolute w-screen max-w-md right-0 z-10 p-5 rounded-md border border-gray-200 shadow-xl bg-white">
+                                            <div className="flex flex-col space-y-3">
+                                                <div className="flex w-full hover:bg-gray-50 p-2 rounded-sm cursor-pointer">
+                                                    <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><img className="rounded-full" src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-09.jpg" width="40" height="40" alt="Burak Long" /></div>
+                                                    <div className="flex flex-col">
+                                                        <span className="font-medium text-gray-600">Burak Long</span>
+                                                        <span className="text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores commodi esse sunt laudantium tempora </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </Popover.Panel>
+                                    </Transition>
+                                </>
+                            )}
+                        </Popover>
+                        <Popover className="relative">
+                            {({ open }) => (
+                                <>
+                                    <Popover.Button className={
+                                        cx(
+                                            'flex items-center justify-center bg-transparent text-gray-400 w-10 h-10 rounded-full',
+                                            open ? 'text-blue-400 hover:text-blue-300' : 'hover:text-gray-300',
+                                        )
+                                    }>
+                                        <ChatIcon className="w-6 h-6" />
+                                    </Popover.Button>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="opacity-0 translate-y-1"
+                                        enterTo="opacity-100 translate-y-0"
+                                        leave="transition ease-in duration-150"
+                                        leaveFrom="opacity-100 translate-y-0"
+                                        leaveTo="opacity-0 translate-y-1">
+                                        <Popover.Panel className="absolute w-screen max-w-md right-0 z-10 p-5 rounded-md border border-gray-200 shadow-xl bg-white">
+                                            <div className="flex flex-col space-y-3">
+                                                <div className="flex w-full hover:bg-gray-50 p-2 rounded-sm cursor-pointer">
+                                                    <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"><img className="rounded-full" src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-09.jpg" width="40" height="40" alt="Burak Long" /></div>
+                                                    <div className="flex flex-col">
+                                                        <span className="font-medium text-gray-600">Burak Long</span>
+                                                        <span className="text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores commodi esse sunt laudantium tempora </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </Popover.Panel>
+                                    </Transition>
+                                </>
+                            )}
+                        </Popover>
                         <button className="flex relative justify-center items-center space-x-1">
                             <img className="rounded-full w-10 h-10" alt="A" src="https://randomuser.me/api/portraits/women/68.jpg" />
                             <ChevronDownIcon className="w-5 h-5 text-gray-200 text-xs" />
