014fa509dc462a18cba65792d1c80930565651c2
Stewie Dong <donghuuhieu1520@gmail.com>
2022-02-10 17:59:38 +0700

    feat: add sidebar navlink chevron
    Signed-off-by: Stewie Dong <donghuuhieu1520@gmail.com>

--- src/components/navlinks/navlink-sidebar-collapse.jsx
+++ src/components/navlinks/navlink-sidebar-collapse.jsx
@@ -14,20 +14,12 @@ import { toggleCollapse } from '@/store/slices/layoutSlice';
 
 export const NavLinkSidebarCollapse = ({ icon: Icon, title, children }) => {
   const dispatch = useDispatch();
-  // const refDisclosureButton = useRef();
   const isSidebarCollapsed = useSelector(selectCollapse);
 
   const navLinkCollapseClass = isSidebarCollapsed ? 'justify-center' : null;
   const hiddenClass = isSidebarCollapsed ? 'hidden' : null;
   const hiddenPadding3 = isSidebarCollapsed ? 'py-3' : 'p-3';
   const hiddenCenter = isSidebarCollapsed ? 'justify-center' : null;
-
-  // useEffect(() => {
-  //   refDisclosureButton.current.addEventListener('click', handleDisclosureButtonClicked);
-  //   return () => {
-  //     refDisclosureButton.current?.removeEventListener('click', handleDisclosureButtonClicked);
-  //   };
-  // });
 
   const handleDisclosureButtonClicked = (e) => {
     if (isSidebarCollapsed) {
@@ -44,14 +36,19 @@ export const NavLinkSidebarCollapse = ({ icon: Icon, title, children }) => {
         as={Fragment}>
         <Disclosure.Button as={Fragment}
         >
-          <button className={cx(navLinkCollapseClass, hiddenPadding3, hiddenCenter, 'flex w-full items-center space-x-4 rounded cursor-pointer hover:bg-gray-100 dark:hover:bg-slate-700')}
-            onClick={handleDisclosureButtonClicked}>
-            <Icon className="w-6 h-6" />
-            <span className={cx('font-medium text-left', hiddenClass)}>{title}</span>
-            <div className={cx(hiddenClass)}>
-              <ChevronDownIcon className="h-4" />
-            </div>
-          </button>
+          {({ open }) => {
+            const className = open ? 'rotate-180' : null;
+            return (
+              <button className={cx(navLinkCollapseClass, hiddenPadding3, hiddenCenter, 'flex w-full items-center space-x-4 rounded cursor-pointer hover:bg-gray-100 dark:hover:bg-slate-700')}
+                onClick={handleDisclosureButtonClicked}>
+                <Icon className="w-6 h-6" />
+                <span className={cx('font-medium text-left', hiddenClass)}>{title}</span>
+                <div className={cx(hiddenClass)}>
+                  <ChevronDownIcon className={cx('h-4 transition-transform ease-in-out delay-150', className)} />
+                </div>
+              </button>
+            );
+          }}
         </Disclosure.Button>
         <Transition
           enter="transition-[max-height] duration-100 ease-in-out"
